066a7efd20e334448f8f0eec2cd3b56cb7ebba4b
kidus-yohannes1 <se.kidus.yohannes@gmail.com>
2021-08-24 15:40:36 +0300

    user model added

---
+++ dating_app/lib/user/models/user.dart
@@ -0,0 +1,36 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+class User extends Equatable {
+  User(
+      {required this.id,
+      required this.username,
+      required this.name,
+      required this.email,
+      required this.password,
+      required this.address});
+
+  final String id;
+  final String username;
+  final String name;
+  final String email;
+  final String password;
+  final String address;
+
+  @override
+  List<Object> get props => [id, username, name, email, password, address];
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+        id: json['id'],
+        username: json['username'],
+        name: json['name'],
+        email: json['email'],
+        password: json['password'],
+        address: json['address']);
+  }
+
+  @override
+  String toString() =>
+      'Course { id: $id, username: $username, name: $name,email: $email, password: $password, address: $address }';
+}
