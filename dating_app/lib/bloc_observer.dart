7394d73cb0b897a420e7748ce637cdbfb0b8a320
gechderib <gechderib@gmail.com>
2021-08-30 19:59:22 +0300

    repository and some screensare added

---
+++ dating_app/lib/bloc_observer.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SampleObsever extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    print(change);
+  }
+
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+    print(event);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print(transition);
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    super.onError(bloc, error, stackTrace);
+    print('Error: $error Stacktrace: $stackTrace');
+  }
+}
--- dating_app/lib/main.dart
+++ dating_app/lib/main.dart
@@ -1,14 +1,51 @@
+import 'package:dating_app/bloc_observer.dart';
+import 'package:dating_app/user/bloc/user_bloc.dart';
+import 'package:dating_app/user/bloc/user_event.dart';
+import 'package:dating_app/user/data_provider/data_provider.dart';
+import 'package:dating_app/user/data_provider/user_data.dart';
+import 'package:dating_app/user/models/models.dart';
+import 'package:dating_app/user/repository/user_repository.dart';
+import 'package:dating_app/user/screens/user_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(MyApp());
+  Bloc.observer = SampleObsever();
+  // UserDataProvider(httpClient: http.Client()).createUser(User(
+  //     id: '1',
+  //     username: 'username',
+  //     name: 'name',
+  //     email: 'email',
+  //     password: 'password',
+  //     address: 'address'));
+  
+  final UserRepository userRepository = UserRepository(
+      userDataProvider: UserDataProvider(httpClient: http.Client()));
+  runApp(DatingApp(
+    userRepository: userRepository,
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class DatingApp extends StatelessWidget {
+  final UserRepository userRepository;
+  DatingApp({required this.userRepository}) : assert(userRepository != null);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return RepositoryProvider.value(
+      value: this.userRepository,
+      child: BlocProvider(
+        create: (context) =>
+            UserBloc(userRepository: this.userRepository)..add(UserLoad()),
+        child: MaterialApp(
+          title: 'Dating App',
+          theme: ThemeData(
+              primaryColor: Colors.blue,
+              visualDensity: VisualDensity.adaptivePlatformDensity),
+          onGenerateRoute: DatingAppRoute.generateRoute,
+        ),
+      ),
+    );
   }
 }
