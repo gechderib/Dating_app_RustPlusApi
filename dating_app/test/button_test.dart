99e3d5ac597ad6a31f6a05152e9565ee1c8de986
gechderib <gechderib@gmail.com>
2021-09-10 20:32:30 +0300

    tasting started

---
+++ dating_app/test/button_test.dart
@@ -0,0 +1,88 @@
+import 'package:dating_app/user/models/user.dart';
+import 'package:dating_app/user/screens/user_login.dart';
+import 'package:dating_app/user/screens/user_route.dart';
+import 'package:dating_app/user/screens/usrchoice.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('flutter test for login page', (WidgetTester tester) async {
+    await tester.pumpWidget(LoginPage());
+    var textField = find.byType(TextFormField);
+    expect(textField, findsOneWidget);
+
+    tester.enterText(textField, 'username');
+    expect(find.text('username'), findsOneWidget);
+    print('username');
+
+    var button = find.text("login");
+    expect(button, findsOneWidget);
+    print('login');
+    await tester.tap(button);
+    await tester.pump();
+    expect(find.text("username"), findsOneWidget);
+    print('username');
+  });
+}
+
+void widgetTest() {
+  testWidgets('find text in widget', (WidgetTester tester) async {
+    await tester.pumpWidget(UserChoicAddUpdate(
+        args: UserArgument(user: User('email', 'password'), edit: true)));
+
+    var textField = find.byType(TextFormField);
+    expect(textField, findsOneWidget);
+
+    tester.enterText(textField, 'username');
+    expect(find.text('username'), findsOneWidget);
+    print('username');
+  });
+}
+
+
+// void main() {
+//   // Define a test. The TestWidgets function also provides a WidgetTester
+//   // to work with. The WidgetTester allows building and interacting
+//   // with widgets in the test environment.
+//   testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
+//     // Create the widget by telling the tester to build it.
+//     await tester.pumpWidget(const MyWidget(title: 'T', message: 'M'));
+
+//     // Create the Finders.
+//     final titleFinder = find.text('T');
+//     final messageFinder = find.text('M');
+
+//     // Use the `findsOneWidget` matcher provided by flutter_test to
+//     // verify that the Text widgets appear exactly once in the widget tree.
+//     expect(titleFinder, findsOneWidget);
+//     expect(messageFinder, findsOneWidget);
+//   });
+// }
+
+
+
+// class MyWidget extends StatelessWidget {
+//   const MyWidget({
+//     Key? key,
+//     required this.title,
+//     required this.message,
+//   }) : super(key: key);
+
+//   final String title;
+//   final String message;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text(title),
+//         ),
+//         body: Center(
+//           child: Text(message),
+//         ),
+//       ),
+//     );
+//   }
+// }
--- dating_app/test/widget_test.dart
+++ dating_app/test/widget_test.dart
@@ -1,14 +1,20 @@
-// // This is a basic Flutter widget test.
-// //
-// // To perform an interaction with a widget in your test, use the WidgetTester
-// // utility that Flutter provides. For example, you can send tap and scroll
-// // gestures. You can also use WidgetTester to find child widgets in the widget
-// // tree, read text, and verify that the values of widget properties are correct.
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
 
-// import 'package:flutter/material.dart';
-// import 'package:flutter_test/flutter_test.dart';
+import 'package:dating_app/user/bloc/user_event.dart';
+import 'package:dating_app/user/models/user.dart';
+import 'package:dating_app/user/screens/addApdate.dart';
+import 'package:dating_app/user/screens/user_detail.dart';
+import 'package:dating_app/user/screens/user_login.dart';
+import 'package:dating_app/user/screens/user_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 
-// import 'package:dating_app/main.dart';
+import 'package:dating_app/main.dart';
 
 // void main() {
 //   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
@@ -28,3 +34,25 @@
 //     expect(find.text('1'), findsOneWidget);
 //   });
 // }
+
+void main() {
+  UserArgument usr = UserArgument(user: User('', ''), edit: true);
+  testWidgets('my widget have a user and a bool value', (WidgetTester tester) async {
+    await tester.pumpWidget(AddUpdateUser(args: usr));
+    final userFinder = find.byElementType(User);
+    final userEditOrUpdate = find.byType(bool);
+
+    expect(userFinder, findsOneWidget);
+    expect(userEditOrUpdate, findsOneWidget);
+  });
+
+    testWidgets('my widget have take a user', (WidgetTester tester) async {
+    await tester.pumpWidget(UserDetail(User('email', 'password')));
+    final userFinder = find.byElementType(User);
+    expect(userFinder, findsOneWidget);
+
+    tester.enterText(, text)
+  });
+
+
+}
